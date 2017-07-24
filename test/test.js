07c9169ad4a2d09bc7cf6816007f4c91ee164eb6
80017 <arpitap@officebeacon.com>
2017-07-24 15:20:44 +0530

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 var request = require('supertest');
 var app = require('../app.js');
 
-describe('GET /', function() {
+describe('GET /a', function() {
   it('respond with hello world', function(done) {
     request(app).get('/').expect('hello world from ob', done);
   });
