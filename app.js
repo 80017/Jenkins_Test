cfc2024aa55da198c15f2d9231a28ba72a16ffd9
Arpita Patel <arpitap@officebeacon.com>
2017-07-28 11:47:12 +0530

    update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let express = require('express');
 let app = express();
 
 app.get('/', function (req, res) {
-  res.send('hello world from ob');
+  res.send('hello world from');
 });
 
 app.listen(process.env.PORT || 4444, function () {
